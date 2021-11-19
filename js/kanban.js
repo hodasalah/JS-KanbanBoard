d5ca2b15429600273542123836d32eed29155847
hodasalah <hodasalah35@gmail.com>
2021-11-20 00:35:02 +0200

    event add

--- js/kanban.js
+++ js/kanban.js
@@ -21,7 +21,7 @@ const create_item = () => {
 	item.id = "item-" + order;
 	item.draggable = true;
 	item.addEventListener("dragstart", (event) => {
-		event.dataTransfer.setData("text", e.target.id);
+		event.dataTransfer.setData("text", event.target.id);
 	});
 	item.addEventListener("dragend", (event) => {
 		event.dataTransfer.clearData();
